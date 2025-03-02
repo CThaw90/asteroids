7870ca13496e942a584c8e24c7c3aa5b2d802737
CThaw90 <cthaw@mediaocean.com>
2025-03-02 17:39:22 -0500

    Created object groups to manage game objects

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ import pygame
 import constants
 
 def main():
-    print('Starting Asteroids!')
-    print('Screen width:', constants.SCREEN_WIDTH)
-    print('Screen height:', constants.SCREEN_HEIGHT)
-
+    pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    Player.containers = (updatable_group, drawable_group)
     player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+    player.containers = (updatable_group, drawable_group)
+
     while True:
         screen.fill('black')
-        player.draw(screen)
+        updatable_group.update(dt)
+        for obj in drawable_group:
+            obj.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
